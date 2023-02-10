d2fca756f3d2b98fecbbdd04f48daafbda4095df
spartakgubarev <spartakgubarev@rambler.ru>
2023-02-10 06:39:11 +0300

    homework lection01 ver 1.0

---
+++ Project01/rnd.java
@@ -0,0 +1,9 @@
+package Project01;
+
+public class rnd {
+    static Integer intRND() {
+        int maxVal = 100;
+        return (int)(Math.random() * maxVal);
+        // System.out.println(rnd);
+    }
+}
