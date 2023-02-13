967c1013750c8e9456c854dbffb6714cf6c52cba
spartakgubarev <spartakgubarev@rambler.ru>
2023-02-13 17:26:35 +0300

    Lection02.2

--- Lection02/Ex002_fileSystemDemo.java
+++ Lection02/Ex002_fileSystemDemo.java
@@ -4,13 +4,13 @@ import java.io.File;
 public class Ex002_fileSystemDemo {
     public static void main(String[] args) {
         String pathProject = System.getProperty("user.dir");
-        String pathFile = pathProject.concat("/file.txt");
+        String pathFile = pathProject.concat("/file666.txt");
         File f3 = new File(pathFile);
         System.out.println(f3.getAbsolutePath());
 
         System.out.println(pathFile);
         File f1 = new File("file.txt");
-        File f2 = new File("/Users/sk/vscode/java_projects/file.txt");
+        File f2 = new File("G:/Учеба/Разработчик/repo/Java/Lection02/file666.txt");
         System.out.println(f1.getAbsolutePath());
         System.out.println(f2.getAbsolutePath());
     }
