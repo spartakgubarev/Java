604f226f3e307bfae8ee1cb93129552541810157
spartakgubarev <spartakgubarev@rambler.ru>
2023-02-20 18:16:04 +0300

    Seminar03 1.0

---
+++ Seminar03/Sem01.java
@@ -0,0 +1,10 @@
+package Seminar03;
+
+public class Sem01 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+// удалил случайно неотслеживаемые файлы, посмотреть еще раз лекцию
