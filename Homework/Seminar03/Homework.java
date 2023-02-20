5d4848f0ef4cc654daab7eb6262fb3bdc81bc3c9
spartakgubarev <spartakgubarev@rambler.ru>
2023-02-20 23:49:03 +0300

    Homework Seminar03 ver 1.1

--- Homework/Seminar03/Homework.java
+++ Homework/Seminar03/Homework.java
@@ -1,7 +1,35 @@
 package Homework.Seminar03;
+
+import java.util.Arrays;;
+
 // +Реализовать алгоритм сортировки слиянием
 public class Homework {
     public static void main(String[] args) {
-        
+        int[] arr = new int[] { 3, 5, 1, 6, 9, 8 };
+        int[] newArr = sort(arr);
+
+        System.err.println(Arrays.toString(arr));
+        System.err.println(Arrays.toString(newArr));
+    }
+
+    // newArray - копирую оригинальный массив
+    // tempArray - создаю хранилище для сортировки
+    // rew -
+    private static int[] sort(int[] array) {
+        int[] newArray = Arrays.copyOf(array, array.length);
+        int[] tempArray = new int[array.length];
+        int[] res = merge(newArray, tempArray, 0, array.length);
+        return res;
+    }
+
+    // startIndex - начальный индекс
+    // endIndex - конечный индекс
+    private static int[] merge(int[] newArray, int[] tempArray, int startIndex, int endIndex) {
+        if (startIndex >= endIndex) {
+            return newArray;
+        }
+
+
+        return newArray;
     }
 }
