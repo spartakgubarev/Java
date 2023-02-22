3fe46585cb4e6b8b71c15d74b8d3731ca1be185c
spartakgubarev <spartakgubarev@rambler.ru>
2023-02-22 22:30:28 +0300

    Seminar03

--- Seminar03/Sem01.java
+++ Seminar03/Sem01.java
@@ -2,7 +2,18 @@ package Seminar03;
 
 public class Sem01 {
     public static void main(String[] args) {
-        
+        int a = 3;
+        int b = 18;
+        int c = 2; // +1
+        int d = 2; // *2
+
+        int[] way = new int[b+1];
+        way[a] = 1;
+
+        for (int i = a+c; i <= b; i++) {
+            way[i] = i % d ==0 ? way[i-c] + way[i/d] : way[i-c];
+        }
+        System.out.println(way[b]);
     }
 }
 
