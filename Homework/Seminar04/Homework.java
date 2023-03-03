2c034b62fbe4d74d7c8ae2387445c0599e1718f6
spartakgubarev <spartakgubarev@rambler.ru>
2023-03-03 18:56:46 +0300

    Homework Seminar 03 ver 1.1

--- Homework/Seminar04/Homework.java
+++ Homework/Seminar04/Homework.java
@@ -1,9 +1,54 @@
 package Homework.Seminar04;
 
+import java.util.Arrays;
+
 public class Homework {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+        int[] arr = new int[] { 12, 11, 13, 5, 6, 7 };
+        System.out.println(Arrays.toString(arr));
+        int size = arr.length;
+
+        heapSort(arr, size);
+    }
+
+    public static void heapSort(int[] myArray, int length) {
+        int temp;
+        int size = length - 1;
+        for (int i = (length / 2); i >= 0; i--) {
+            heapify(myArray, i, size);
+        }
+        for (int i = size; i >= 0; i--) {
+            temp = myArray[0];
+            myArray[0] = myArray[size];
+            myArray[size] = temp;
+            size--;
+            heapify(myArray, 0, size);
+        }
+        System.out.println(Arrays.toString(myArray));
+    }
+
+    public static void heapify(int[] myArray, int i, int heapsize) {
+        int l = 2 * i;
+        int r = 2 * i + 1;
+        int largestElement;
+
+        if (l <= heapsize && myArray[l] > myArray[i]) {
+            largestElement = l;
+        } else {
+            largestElement = i;
+        }
+
+        if (r <= heapsize && myArray[r] > myArray[largestElement]) {
+            largestElement = r;
+        }
+
+        if (largestElement != i) {
+            int temp = myArray[i];
+            myArray[i] = myArray[largestElement];
+            myArray[largestElement] = temp;
+            heapify(myArray, largestElement, heapsize);
+        }
     }
 }
