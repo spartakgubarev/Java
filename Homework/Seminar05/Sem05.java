e887e49c3ee65fb03c200e0ea691691043b0bc3f
spartakgubarev <spartakgubarev@rambler.ru>
2023-03-09 20:50:32 +0300

    Homework05 ver 1.0 map, print map

--- Homework/Seminar05/Sem05.java
+++ Homework/Seminar05/Sem05.java
@@ -1,8 +1,32 @@
 package Homework.Seminar05;
 
 public class Sem05 {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    int[][] labyrinth = getMap();
+    printMap(labyrinth);
+  }
 
+  // Создание карты
+  public static int[][] getMap() {
+    return new int[][] {
+        { -1, -1, -1, -1, -1, -1, -1 },
+        { -1, 10, 10, 10, 10, 10, -1 },
+        { -1, 10, 10, 10, 10, 10, -1 },
+        { -1, 10, 10, 10, 10, 10, -1 },
+        { -1, 10, 10, 10, 10, 10, -1 },
+        { -1, 10, 10, 10, 10, 10, -1 },
+        { -1, -1, -1, -1, -1, -1, -1 },
+    };
+  }
 
+  // Рисование карты
+  public static void printMap(int[][] map) {
+    for (int row = 0; row < map.length; row++) {
+      for (int col = 0; col < map[row].length; col++) {
+        System.out.print(map[row][col]);
+      }
+    System.out.println();
+    }
+  }
 
 }
