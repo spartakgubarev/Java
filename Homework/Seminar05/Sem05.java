3e04f59456cd7a7a99e6eeac3149ef7ee5c3b9c6
spartakgubarev <spartakgubarev@rambler.ru>
2023-03-10 18:57:01 +0300

    Homework05 ver 1.1 map, print map, color

--- Homework/Seminar05/Sem05.java
+++ Homework/Seminar05/Sem05.java
@@ -4,18 +4,27 @@ public class Sem05 {
   public static void main(String[] args) {
     int[][] labyrinth = getMap();
     printMap(labyrinth);
+    labyrinth = colorMap(labyrinth);
+
   }
 
-  // Создание карты
+  // Создание карты (-1 стена), (-9 пустота), (-2 вход), (-3 выход)
   public static int[][] getMap() {
     return new int[][] {
-        { -1, -1, -1, -1, -1, -1, -1 },
-        { -1, 10, 10, 10, 10, 10, -1 },
-        { -1, 10, 10, 10, 10, 10, -1 },
-        { -1, 10, 10, 10, 10, 10, -1 },
-        { -1, 10, 10, 10, 10, 10, -1 },
-        { -1, 10, 10, 10, 10, 10, -1 },
-        { -1, -1, -1, -1, -1, -1, -1 },
+        { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+        { -1, -9, -9, -9, -9, -9, -9, -9, -9, -9, -1, -9, -9, -9, -9, -9, -1 },
+        { -1, -9, -3, -9, -9, -9, -9, -1, -9, -9, -9, -9, -9, -9, -9, -9, -1 },
+        { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -9, -9, -9, -9, -9, -1 },
+        { -1, -9, -9, -9, -9, -9, -9, -9, -1, -9, -9, -9, -9, -9, -9, -9, -1 },
+        { -1, -9, -9, -9, -9, -9, -9, -9, -1, -9, -9, -9, -9, -9, -9, -9, -1 },
+        { -1, -9, -1, -9, -9, -9, -9, -9, -1, -9, -9, -9, -9, -9, -9, -9, -1 },
+        { -1, -9, -1, -1, -9, -9, -9, -9, -1, -9, -9, -9, -9, -9, -9, -9, -1 },
+        { -1, -9, -9, -1, -9, -9, -9, -9, -1, -1, -1, -1, -1, -9, -9, -9, -1 },
+        { -1, -9, -9, -1, -9, -9, -9, -9, -1, -9, -9, -9, -9, -9, -9, -9, -1 },
+        { -1, -9, -9, -1, -9, -9, -9, -9, -1, -9, -9, -9, -9, -9, -9, -9, -1 },
+        { -1, -9, -9, -1, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -1 },
+        { -1, -9, -2, -1, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -9, -1 },
+        { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
     };
   }
 
@@ -23,10 +32,47 @@ public class Sem05 {
   public static void printMap(int[][] map) {
     for (int row = 0; row < map.length; row++) {
       for (int col = 0; col < map[row].length; col++) {
-        System.out.print(map[row][col]);
+        System.out.print(map[row][col] + " ");
       }
-    System.out.println();
+      System.out.println();
     }
+  }
+
+  // Раскраска карты
+  public static int[][] colorMap(int[][] mapLab) {
+    for (int row = 0; row < mapLab.length; row++) {
+      for (int col = 0; col < mapLab[row].length; col++) {
+        switch (mapLab[row][col]) {
+          case -1:
+            System.out.print("#" +" ");
+            break;
+            case -9:
+            System.out.print(" "+" ");
+            break;
+            case -2:
+            System.out.print("@"+" ");
+            break;
+            case -3:
+            System.out.print("&"+" ");
+            break;
+
+          default:
+            break;
+        }
+        // if (mapLab[row][col] == -1) {
+        //   System.out.println("#");
+        // }
+        // if (mapLab[row][col] == -9) {
+        //   System.out.println(" ");
+        // }
+        // if (mapLab[row][col] == -) {
+        //   System.out.println("#");
+        // }
+
+      }
+      System.out.print("\n");
+    }
+    return mapLab;
   }
 
 }
