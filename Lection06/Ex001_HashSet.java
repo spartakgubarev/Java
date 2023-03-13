45569d7a71bcafa2793b6f07002a3877b3539aab
spartakgubarev <spartakgubarev@rambler.ru>
2023-03-13 17:48:32 +0300

    Lection06

---
+++ Lection06/Ex001_HashSet.java
@@ -0,0 +1,36 @@
+package Lection06;
+
+import java.util.*;
+
+public class Ex001_HashSet {
+public static void main(String[] args) {
+    Set<Integer> s = new HashSet<>();
+    s.add(1);
+    s.add(2);
+    s.add(11);
+    s.add(4);
+    s.add(3);
+    s.add(null);
+
+    System.out.println(s);
+    }
+
+
+    // public static void main(String[] args) {
+    //     Set<Integer> set = new HashSet<>();
+    //     set.add(1); set.add(12); set.add(123);
+    //     set.add(1234); set.add(1234);
+    //     System.out.println(set.contains(12)); // true
+    //     set.add(null);
+    //     System.out.println(set.size()); // 5
+    //     System.out.println(set); // [null, 1, 1234, 123, 12]
+    //     set.remove(12);
+    //     for (var item : set) {
+    //         System.out.println(item); // null 1 1234 123
+    //     }
+    //     set.clear();
+    //     System.out.println(set); // []
+    // }
+
+
+}
